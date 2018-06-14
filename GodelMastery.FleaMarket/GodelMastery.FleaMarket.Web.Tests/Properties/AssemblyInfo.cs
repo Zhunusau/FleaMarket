7b3204a4cf6676e4e726de80d136c445ad4b1e99
Ruslan Zhunusau <zhunusau@gmail.com>
2018-06-14 19:04:40 +0300

    added UnitTest projects to sln

---
+++ GodelMastery.FleaMarket/GodelMastery.FleaMarket.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GodelMastery.FleaMarket.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GodelMastery.FleaMarket.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec359436-2258-483a-94bc-bbca8657eb54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
