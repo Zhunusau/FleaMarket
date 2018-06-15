e59aaf812712466dfa36c754369328965708740b
Vlad <32732269+vladkuzmich@users.noreply.github.com>
2018-06-15 10:33:18 +0300

    [23]: [DAL]: Implement UnitOfWork pattern (#2)
    [23]: [DAL]: Implement UnitOfWork pattern

---
+++ GodelMastery.FleaMarket/GodelMastery.FleaMarket.DAL/Core/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using GodelMastery.FleaMarket.DAL.Interfaces;
+
+namespace GodelMastery.FleaMarket.DAL.Core
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext dbContext;
+        private bool disposed = false;
+
+        public async Task SaveChanges()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+
+        public void RollBack()
+        {
+            foreach (var entry in dbContext.ChangeTracker.Entries()
+                .Where(e => e.State != EntityState.Unchanged))
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Modified:
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    dbContext.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ GodelMastery.FleaMarket/GodelMastery.FleaMarket.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GodelMastery.FleaMarket.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task SaveChanges();
+        void RollBack();
+    }
+}
